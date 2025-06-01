edfa17c82978e19cc3edd09ea95d3c0164c7ed9d
Kosuke Matsuda <>
2025-06-01 17:24:46 +0900

    Add navigation

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -1,9 +1,9 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View } from 'react-native';
 
-export default function Index() {
+export default function AboutScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
+      <Text style={styles.text}>About screen</Text>
     </View>
   );
 }
